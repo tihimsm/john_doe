9cd5c1b51da94f07c1b97aabfb4a7c3046817dce
三島 太平 <mishimataihei@LAB-N1009.local>
2016-09-15 21:14:03 +0900

    動きつけた

--- js/main.js
+++ js/main.js
@@ -1,67 +1,40 @@
 $(function(){
   $('body').transition({opacity: 1}, 3000, 'in-out');
 
-  var body_flag = false;
+  var image_flag = false;
   var menu_flag = true;
 
-  menuHover('#menu_button');
-  menuOpen();
+  titleHover('.title');
 
-  $('body').click(function(e){
-    if(body_flag){
-      var target = e.target;
-      if (target.id === 'menu_list') {
-        return false;
-      }
-      body_flag = false;
-      $('#menu_list li a').transition({
-        opacity: 0
-      }, function(){
-        $('#menu_list').transition({
-          width: 0,
-          backgroundColor: 'rgba(80, 80, 80, 0)'
-        },function(){
-          menu_flag = true;
-          $.when($(this).remove())
-          .then($('#menu_inner').prepend('<div id="menu_button" style="left: -150px; background-color: rgba(80, 80, 80, 0); width: 150px; height: 0; transform: rotate(90deg);"></div>'))
-          .then($('#menu_button').prepend('<a style="">MENU</a>'))
-          .then($('#menu_button').transition({
-            rotate: '0deg',
-            left: 0,
-            width: 300,
-            height: 50,
-            backgroundColor: 'rgba(80, 80, 80, 0.2)'
-          },function(){
-            menuHover('#menu_button');
-            menuOpen();
-          }));
-        });
-      });
-    }else{
-      return false;
-    }
-  });
-
-  function menuHover(menu){
-    $(menu).hover(
+  function titleHover(title){
+    $(title).hover(
       function(){
-        $(this).transition({
-          backgroundColor: 'rgba(80, 80, 80, 0.7)'
-        },300,'ease');
-        if(menu === '#menu_button') {
-          $(this).css("cursor","pointer");
-          $('#menu_button a').transition({
-            color: '#fff'
-          },300,'ease');
+        if(!image_flag){
+          image_flag = true;
+          $('#message').transition({
+            opacity: 0
+          }, 100, function(){
+            $('#rightContent').prepend('<div id="test"></div>');
+            $('#test').transition({
+              width: '100%',
+              right:0
+            }, 300, 'cubic-bezier(1,0,0.38,1)');
+          });
         }
       },
       function(){
-        $(this).transition({
-          backgroundColor: 'rgba(80, 80, 80, 0.2)'
-        },300,'ease');
-        $('#menu_button a').transition({
-          color: '#000'
-        },300,'ease');
+        $('#test').transition({
+          width: 0,
+          opacity: 0,
+          right: '50%'
+        }, 500, function(){
+          $.when($('#test').remove())
+          .then($('#message').transition({
+            opacity: 1
+          }, 300, 'cubic-bezier(1,0,0.38,1)', function(){
+            image_flag = false;
+          }));
+        });
       }
     );
   }
