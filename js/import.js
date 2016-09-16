bb5ac1dc51f62f57cd6ca9f1f75a15169f4b2c7f
三島 太平 <mishimataihei@LAB-N1009.local>
2016-09-16 16:53:12 +0900

    一時コミット

---
+++ js/import.js
@@ -0,0 +1,3 @@
+document.write('<script src="js/jquery.min.js"></script>');
+document.write('<script src="js/jquery.transit.js"></script>');
+document.write('<script src="js/main.js"></script>');
--- js/main.js
+++ js/main.js
@@ -2,15 +2,17 @@ $(function(){
   $('body').transition({opacity: 1}, 3000, 'in-out');
 
   var image_flag = false;
+  var detail_flag = false;
 
   titleHover('.title');
+  titleClick('.title');
 
   function titleHover(title){
     $(title).hover(
       function(){
         var id = $(this).attr('id');
         var id_str = 'img/image0' + id + '.jpg';
-        if(!image_flag){
+        if(!image_flag && !detail_flag){
           image_flag = true;
           $('#message').transition({
             opacity: 0
@@ -25,19 +27,59 @@ $(function(){
         }
       },
       function(){
-        $('#test').transition({
-          opacity: 0,
-          scale: [1, 0]
-        }, 400, 'cubic-bezier(1,0,0.38,1)', function(){
-          $.when($('#test').remove())
-          .then($('#message').transition({
-            opacity: 1
-          }, 200, function(){
-            image_flag = false;
-          }));
-        });
+        if(!detail_flag){
+          $('#test').transition({
+            opacity: 0,
+            scale: [1, 0]
+          }, 400, 'cubic-bezier(1,0,0.38,1)', function(){
+            $.when($('#test').remove())
+            .then($('#message').transition({
+              opacity: 1
+            }, 200, function(){
+              image_flag = false;
+            }));
+          });
+        }
       }
     );
+  }
+
+  function titleClick(title){
+    $(title).click(function(){
+      detail_flag = true;
+      $('#test').transition({
+        opacity: 0,
+        scale: [1, 0]
+      }, 400, 'cubic-bezier(1,0,0.38,1)', function(){
+        $.when($('#test').remove())
+        .then(function(){
+          image_flag = false;
+          $('header').transition({
+            opacity: 0,
+          }, 200);
+          $('#container').transition({
+            opacity: 0,
+          }, 200, function(){
+            detail_indicate();
+            $('#detail_container').transition({
+              marginTop: 0,
+              opacity: 1,
+            }, 1000, function(){
+              $('header').transition({
+                scale: 0
+              }, 200);
+              $('#container').transition({
+                height: 0
+              }, 200);
+            });
+          });
+        });
+      });
+    });
+  }
+
+  function detail_indicate(){
+    $('body').prepend('<div id="detail_container"><div id="det_header"><div id="logo"><span>PicaVinchi</span></div></div></div>');
   }
 
 });
