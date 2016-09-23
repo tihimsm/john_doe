ce6ed91e723d22d637f5b096fde429a326866561
三島 太平 <mishimataihei@LAB-N1009.local>
2016-09-23 14:18:09 +0900

    作品表示内容をjsonに切り分け

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 $(window).load(function(){
   $('body').transition({opacity: 1}, 3000, 'in-out');
 
+  var articles = {};
+
+  $.ajax({
+    url: "json/article.json",
+    dataType: "json",
+    async: false,
+    success: function(json) {
+      articles = json;
+    }
+  });
+
+  var id = 0;
+  for (var article of articles) {
+    id++;
+    $('#leftContent').append('<section class="works"><h1 class="title"><span id="'+ id +'">'+ article["work_title"] +'</span></h1><p>0'+ id +'</p></section>');
+  }
+
   var image_flag = false;
   var detail_flag = false;
 
@@ -62,7 +79,6 @@ $(window).load(function(){
           }, 200);
           $('#container').transition({
             opacity: 0,
-            // scale: [1, 0]
           }, 500, function(){
             detail_indicate(id);
             $('#detail_container').transition({
@@ -87,7 +103,6 @@ $(window).load(function(){
         }, 200);
         $('#container').transition({
           opacity: 1,
-          // scale: [1, 1]
         }, 500);
         $('#message').transition({
           opacity: 1
@@ -99,71 +114,12 @@ $(window).load(function(){
   }
 
   function detail_indicate(num){
-    num -= 1;
-
-    var work_title = [
-      "kujaku 2016AW creation",
-      "Sassoon show movie",
-      "coming soon",
-      "coming soon"
-    ];
-
-    var str = [
-      [
-        "If you really want to hear about it,",
-        "the first thing you'll probably want to know is where",
-        "I was born,",
-        "an what my lousy childhood was like,",
-        "and how my parents were occupied and all before",
-        "they had me,",
-        "and all that David Copperfield kind of crap,",
-        "but I don't feel like going into it,",
-        "if you want to know the truth."
-      ],
-      [
-        "How happy I am that I am gone!",
-        "My dear friend, what a thing is the heart of man!",
-        "To leave you, from whom I have been inseparable,",
-        "whom I love so dearly, and yet to feel happy!",
-        "I know you will forgive me.",
-        "Have not other attachments been specially appointed",
-        "by fate to torment a head like mine?",
-        "Poor Leonora! and yet I was not to blame."
-      ],
-      [
-        "On an exceptionally hot evening early in July",
-        "a young man came out of the garret in which he lodged in S.",
-        "Place and walked slowly,",
-        "as though in hesitation, towards K. bridge.",
-        "He had successfully avoided",
-        "meeting his landlady on the staircase.",
-        "His garret was under the roof of a high,",
-        "five-storied house and was more like a cupboard than a room."
-      ],
-      [
-        "If you really want to hear about it,",
-        "the first thing you'll probably want to know is where",
-        "I was born,",
-        "an what my lousy childhood was like,",
-        "and how my parents were occupied and all before",
-        "they had me,",
-        "and all that David Copperfield kind of crap,",
-        "but I don't feel like going into it,",
-        "if you want to know the truth."
-      ]
-    ];
-
-    var movie = [
-      "<iframe width='853' height='480' src='https://www.youtube.com/embed/PNrXEDKEinI' frameborder='0' allowfullscreen></iframe>",
-      "<iframe width='853' height='480' src='https://www.youtube.com/embed/2OwnjfCXBos' frameborder='0' allowfullscreen></iframe>",
-      "<iframe width='853' height='480' src='https://www.youtube.com/embed/s1eEFrGUOsw' frameborder='0' allowfullscreen></iframe>",
-      "<iframe width='853' height='480' src='https://www.youtube.com/embed/oCrwzN6eb4Q?list=RDoCrwzN6eb4Q' frameborder='0' allowfullscreen></iframe>"
-    ]
+    var detail = articles[num-1];
     $('body').prepend('<div id="detail_container"><div id="det_header"><div id="det_logo"><span>PicaVinci</span></div><div id="close"><span>close</span><div></div></div>');
-    $('#detail_container').append('<div id="det_main"><div id="movie">' + movie[num]+ '</div></div>');
-    $('#det_main').append('<div id="work"><div id="work_title"><h2>' + work_title[num] + '</h2></div><div id="description"></div></div>');
-    for(var i=0; i<str[num].length; i++){
-      $('#description').append('<p>'+ str[num][i] +'</p>');
+    $('#detail_container').append('<div id="det_main"><div id="movie"><iframe width="853" height="480" src="' + detail["movie"] + '" frameborder="0" allowfullscreen></iframe></div></div>');
+    $('#det_main').append('<div id="work"><div id="work_title"><h2>' + detail["work_title"] + '</h2></div><div id="description"></div></div>');
+    for(var i=0; i<detail["str"].length; i++){
+      $('#description').append('<p>'+ detail["str"][i] +'</p>');
     }
   }
 
