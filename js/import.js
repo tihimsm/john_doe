d48433f186afc786bb58d03f552d92cfb6ca6cdf
三島 太平 <mishimataihei@LAB-N1009.local>
2016-09-23 16:11:08 +0900

    sp用js追加

--- js/import.js
+++ js/import.js
@@ -1,3 +1,7 @@
 document.write('<script src="js/jquery.min.js"></script>');
 document.write('<script src="js/jquery.transit.js"></script>');
-document.write('<script src="js/main.js"></script>');
+if ( window.innerWidth > 481) {
+  document.write('<script src="js/main.js"></script>');
+} else {
+  document.write('<script src="js/smart.js"></script>');
+}
