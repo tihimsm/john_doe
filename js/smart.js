9d815a29a96d0428606ae7ae1a843601000a93d1
三島 太平 <mishimataihei@LAB-N1009.local>
2016-09-23 17:03:27 +0900

    sp用詳細画面修正

--- js/smart.js
+++ js/smart.js
@@ -1,5 +1,5 @@
 $(window).load(function(){
-  $('body').transition({opacity: 1}, 3000, 'in-out');
+  $('body').transition({opacity: 1}, 1000, 'in-out');
 
   var articles = {};
 
@@ -35,106 +35,56 @@ $(window).load(function(){
         stickyFlag = false;
       }
     }
-    console.log($('#1').offset().top);
   });
 
-  // var image_flag = false;
-  // var detail_flag = false;
+  $('.title span').click(function(){
+    titleClick(this);
+  });
+  closeDetail('#close span');
 
-  // titleHover('.title span');
-  // titleClick('.title span');
-  // closeDetail('#close span');
+  function titleClick(title){
+    var id = $(title).attr('id');
+    detail_flag = true;
+    image_flag = false;
+    $('header').transition({
+      opacity: 0,
+    }, 200);
+    $('#container').transition({
+      opacity: 0,
+    }, 500, function(){
+      detail_indicate(id);
+      $('#detail_container').transition({
+        scale: [1, 1],
+        opacity: 1,
+      }, 300);
+    });
+  }
 
-  // function titleHover(title){
-  //   $(title).hover(
-  //     function(){
-  //       var id = $(this).attr('id');
-  //       var id_str = 'img/0' + id + '.jpg';
-  //       if(!image_flag && !detail_flag){
-  //         image_flag = true;
-  //         $('#message').transition({
-  //           opacity: 0
-  //         }, 100, function(){
-  //           $('#rightContent').prepend('<div id="test"></div>');
-  //           $('#test').css("background-image", "url('" + id_str +"')");
-  //           $('#test').transition({
-  //             opacity: 1,
-  //             scale: [1, 1]
-  //           }, 300, 'cubic-bezier(1,0.5,0.38,1)', function(){
-  //           });
-  //         });
-  //       }
-  //     },
-  //     function(){
-  //       if(!detail_flag){
-  //         $('#test').transition({
-  //           opacity: 0,
-  //           scale: [1, 0]
-  //         }, 400, 'cubic-bezier(1,0,0.38,1)', function(){
-  //           $.when($('#test').remove())
-  //           .then($('#message').transition({
-  //             opacity: 1
-  //           }, 200, function(){
-  //             image_flag = false;
-  //           }));
-  //         });
-  //       }
-  //     }
-  //   );
-  // }
-
-  // function titleClick(title){
-  //   $(title).click(function(){
-  //     var id = $(this).attr('id');
-  //     detail_flag = true;
-  //     $('#test').transition({
-  //       opacity: 0,
-  //       scale: [1, 0]
-  //     }, 400, 'cubic-bezier(1,0,0.38,1)', function(){
-  //       $.when($('#test').remove())
-  //       .then(function(){
-  //         image_flag = false;
-  //         $('header').transition({
-  //           opacity: 0,
-  //         }, 200);
-  //         $('#container').transition({
-  //           opacity: 0,
-  //         }, 500, function(){
-  //           detail_indicate(id);
-  //           $('#detail_container').transition({
-  //             scale: [1, 1],
-  //             opacity: 1,
-  //           }, 500);
-  //         });
-  //       });
-  //     });
-  //   });
-  // }
-
-  // function closeDetail(close){
-  //   $(document).on('click', close, function(){
-  //     $('#detail_container').transition({
-  //       scale: [1, 0],
-  //       opacity: 0
-  //     }, 500, function(){
-  //       $(this).remove();
-  //       $('header').transition({
-  //         opacity: 1,
-  //       }, 200);
-  //       $('#container').transition({
-  //         opacity: 1,
-  //       }, 500);
-  //       $('#message').transition({
-  //         opacity: 1
-  //       }, 500);
-  //       image_flag = false;
-  //       detail_flag= false;
-  //     });
-  //   });
-  // }
+  function closeDetail(close){
+    $(document).on('click', close, function(){
+      $('#detail_container').transition({
+        scale: [1, 0],
+        opacity: 0
+      }, 300, function(){
+        $(this).remove();
+        $('header').transition({
+          opacity: 1,
+        }, 200);
+        $('#container').transition({
+          opacity: 1,
+        }, 500);
+        $('#message').transition({
+          opacity: 1
+        }, 500);
+        image_flag = false;
+        detail_flag= false;
+      });
+    });
+  }
 
   function detail_indicate(num){
     var detail = articles[num-1];
+    console.log(detail);
     $('body').prepend('<div id="detail_container"><div id="det_header"><div id="det_logo"><span>PicaVinci</span></div><div id="close"><span>close</span><div></div></div>');
     $('#detail_container').append('<div id="det_main"><div id="movie"><iframe width="853" height="480" src="' + detail["movie"] + '" frameborder="0" allowfullscreen></iframe></div></div>');
     $('#det_main').append('<div id="work"><div id="work_title"><h2>' + detail["work_title"] + '</h2></div><div id="description"></div></div>');
