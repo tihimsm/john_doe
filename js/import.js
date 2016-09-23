e3f69938f3294b145e314d782e2379c515d88aa6
三島 太平 <mishimataihei@LAB-N1009.local>
2016-09-23 19:08:09 +0900

    css,js切り替え幅変更

--- js/import.js
+++ js/import.js
@@ -1,6 +1,12 @@
 document.write('<script src="js/jquery.min.js"></script>');
 document.write('<script src="js/jquery.transit.js"></script>');
-if ( window.innerWidth > 481) {
+// if ( window.innerWidth > 481) {
+//   document.write('<script src="js/main.js"></script>');
+// } else {
+//   document.write('<script src="js/smart.js"></script>');
+// }
+
+if ( window.innerWidth > 771) {
   document.write('<script src="js/main.js"></script>');
 } else {
   document.write('<script src="js/smart.js"></script>');
